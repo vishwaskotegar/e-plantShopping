6570ed3020f2b831d2245cf9557b2f065a2d89d5
vishwas <vishwas@gmail.com>
2025-06-12 03:35:21 -0400

    updates

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,11 +9,11 @@ const CartItem = ({ onContinueShopping }) => {
 
     // Calculate total amount for all products in the cart
     const calculateTotalAmount = () => {
-        var total;
+        var total = 0;
         cart.forEach((item) => {
             const quantity = item.quantity
             const cost = parseFloat(item.cost.substring(1))
-            total = cost * quantity
+            total += cost * quantity
         })
         return total
 
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+import { useDispatch } from 'react-redux';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -261,6 +264,8 @@ function ProductList({ onHomeClick }) {
           ...prevState, // Spread the previous state to retain existing entries
           [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
         }));
+
+        console.log(addedToCart)
       };
     return (
         <div>
@@ -300,12 +305,12 @@ function ProductList({ onHomeClick }) {
                                         <div className="product-title">{plant.name}</div> {/* Display plant name */}
                                         {/* Display other plant details like description and cost */}
                                         <div className="product-description">{plant.description}</div> {/* Display plant description */}
-                                        <div className="product-cost">${plant.cost}</div> {/* Display plant cost */}
+                                        <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
                                         <button
-                                            className="product-button"
+                                            className={!addedToCart[plant.name]? "product-button": "product-button added-to-cart"} disabled={addedToCart[plant.name]}
                                             onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
                                         >
-                                            Add to Cart
+                                            {addedToCart[plant.name] ? 'Added to Cart' : 'Add to Cart'}
                                         </button>
                                     </div>
                                 ))}
